79e1f491ca8e28fc6541019797d3ba7d0010ea19
ZAHIDHOSEN <zahidhosen203@gmail.com>
2026-06-17 21:01:31 +0600

    added server.ts file with slove vercel sleeping problem

--- dist/server.js
+++ dist/server.js
@@ -17,16 +17,26 @@ const app_1 = __importDefault(require("./app"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 let server;
+let isConnected = false;
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (isConnected) {
+        return;
+    }
     try {
-        yield mongoose_1.default.connect(process.env.DB_URL);
-        console.log("connect to db");
-        server = app_1.default.listen((process.env.PORT), () => {
-            console.log(`server is listening on port:${process.env.PORT}`);
+        yield mongoose_1.default.connect(process.env.DB_URL, {
+            bufferCommands: false
         });
+        isConnected = true;
+        console.log("connect to db");
+        if (process.env.NODE_ENV !== "production") {
+            server = app_1.default.listen((process.env.PORT), () => {
+                console.log(`server is listening on port:${process.env.PORT}`);
+            });
+        }
     }
     catch (error) {
         throw new Error("error in start server");
     }
 });
 startServer();
+exports.default = app_1.default;
--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,27 @@ import dotenv from "dotenv"
 dotenv.config()
 
 let server:Server
-
+let isConnected:any = false
 
 const startServer = async()=>{
+   if(isConnected){
+      return
+   }
+
     try {
-       await mongoose.connect(process.env.DB_URL as string)
+       await mongoose.connect(process.env.DB_URL as string,{
+         bufferCommands:false
+      });
+       isConnected = true
        console.log("connect to db")
 
-
-    server = app.listen((process.env.PORT),()=>{
+   if(process.env.NODE_ENV !=="production"){
+      server = app.listen((process.env.PORT),()=>{
       console.log(`server is listening on port:${process.env.PORT}`)
     })
-
-
-        
+   }
+   
+    
     } catch (error) {
        throw new Error("error in start server")
       
@@ -30,4 +37,8 @@ const startServer = async()=>{
 
 
 startServer()
+
+
+
+export default app
 
